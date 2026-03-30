ffb2de8247e5ed3abccfa560d66f3e692dd15947
aisha <aesha0507@gmail.com>
2026-03-30 12:00:46 +0200

    Fixing CSS problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title', 'Shopify - Ecommerce Store')</title>
 
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
     @stack('styles')
